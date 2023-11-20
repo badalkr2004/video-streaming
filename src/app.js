e012976bf47010944131710d7625a65e9812d6e3
badalkr2004 <badalkr279@gmail.com>
2023-11-20 21:11:43 +0530

    controllers and routes setup

--- src/app.js
+++ src/app.js
@@ -10,10 +10,17 @@ app.use(
     credentials: true,
   })
 );
-
+0
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//route declaration
+app.use("/api/v1/users",userRouter)
 
 export { app };
