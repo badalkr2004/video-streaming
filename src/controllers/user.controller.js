986385837cdf457f003a93d09669b18a17587a2e
badalkr2004 <badalkr279@gmail.com>
2023-12-17 18:49:09 +0530

    postman setup and some code changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,13 @@ const registerUser = asyncHandler(async (req,res)=>{
         }
 
         const avatarLocalPath = req.files?.avatar[0]?.path
-        const coverImageLocalPath = req.files?.coverImage[0]?.path
+        // const coverImageLocalPath = req.files?.coverImage[0]?.path  
+        
+        let coverImageLocalPath;
+        if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+            coverImageLocalPath = req.files.coverImage[0].path
+        }
+
 
         if(!avatarLocalPath){
             throw new ApiError(400,"Avatar file is required")
@@ -36,12 +42,12 @@ const registerUser = asyncHandler(async (req,res)=>{
         }
 
         const user = await User.create({
+            username,
             fullName,
             avatar: avatar.url,
             coverImage: coverImage?.url || "",
             email,
             password,
-            username:username.toLowerCase()
         })
 
         const createdUser = await User.findById(user._id).select("-password -refreshToken")
